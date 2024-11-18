557bc1dcabecead0669da60f18f8827467d23cd4
Artik Zenevich <azenevich91@gmail.com>
2024-11-18 10:42:19 +0300

    init project

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '54ed0c117ae19ee2db60');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '7091d15c08189d2475ab');
